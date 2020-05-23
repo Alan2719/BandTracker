429fbbf854da0d40513028982875788236690891
Alan Velazquez <alan2719.ahcv@gmail.com>
2020-05-23 09:47:39 -0500

    Responsive fixed

---
+++ events.js
@@ -0,0 +1,143 @@
+var bandName = "";
+var bandsArray = [];
+var allVenues = [];
+var buttonTickets;
+
+//Conditon to get the empty array or the array with the current
+//artists.
+if(localStorage.getItem('followBand') === null) {
+    bandsArray = [];
+} else {
+    bandsArray = localStorage.getItem('followBand');
+}
+console.log(localStorage.getItem('followBand'));
+
+//API to append the image, and band name
+$("#bandSearch").on('click',function(){
+    $("tbody").empty();
+    $("#artistName").empty();
+    $("#fblink").empty();
+    $('#bandImage').empty();
+    $("#error-container").css('display','none');
+    console.log($("#icon_prefix").val());
+    if ($("#icon_prefix").val() !== "") {
+        bandName = $("#icon_prefix").val();
+        console.log(bandName);
+        localStorage.setItem("bandName", bandName);
+        var apiKEY = "105d4ded77051c3ef0322214703f7bb8";
+        var queryURL = "https://rest.bandsintown.com/artists/" + bandName + "/?app_id=" + apiKEY;
+        console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function(response){
+            if (response.name) {
+                $("#container").css('display','block');
+                artistName = response.name;
+                var website = response.facebook_page_url;
+                var bandImage = response.image_url;
+                $(".artistNameSpan").text(artistName);
+                $("#fblink").attr('href',website);
+                $('#bandImage').attr('src',bandImage);
+            } else {
+                //Error message in case the band is not found
+                $("#container").css('display','none');
+                $("#error-container").css('display','block');
+                $("#error-container").text("Sorry, band not found");
+            }
+        })
+    } else {
+        //Error message in case the input has no name
+        $("#container").css('display','none');
+        $("#error-container").css('display','block');
+        $("#error-container").text("Enter a valid name");
+    }
+    //API to get the upcoming events
+    var queryURL2 = "https://rest.bandsintown.com/artists/" + bandName + "/events/?app_id=" + apiKEY;
+    console.log(queryURL2);
+    allVenues = [];
+    $.ajax({
+        url: queryURL2,
+        method: 'GET'
+    }).then(function(events){
+        console.log(events);
+        for (var i = 0; i < 5; i++) {
+            var eventDate = events[i].datetime;
+            var eventVenue = events[i].venue.name;
+            var location = events[i].venue.city;
+            var eventCountry = events[i].venue.country;
+
+            allVenues.push(eventVenue);
+            console.log(allVenues);
+            var row = $("<tr>")
+            var date = $("<td>").text(eventDate);
+            var venue = $("<td>").text(eventVenue);
+            var city = $("<td>").text(location);
+            var country = $("<td>").text(eventCountry);
+            row.append(date,venue,city,country);
+            $("tbody").append(row);
+        }
+        //API to get similar artists, listeners and playcount
+        console.log(bandName);
+        var apiKEY2 = "bed52730fbe111f6add92609042505dc";
+        var queryURL3 = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + bandName + "&api_key=" + apiKEY2 + "&format=json";
+        console.log(queryURL3);
+        $("#eachBand").empty();
+        $.ajax({
+            url: queryURL3,
+            method: 'GET'
+        }).then(function(information) {
+            console.log(information);
+            $("#listeners").text(information.artist.stats.listeners);
+            $("#playcount").text(information.artist.stats.playcount);
+            for (var j = 0; j < 4; j++){
+                //var img = $("<img>");
+                var similarArtist = $("<p>");
+                similarArtist.addClass("text");
+                var container = $("<div>");
+                var artists = similarArtist.text(information.artist.similar.artist[j].name);
+                //var bandImage = img.attr('src', information.artist.similar.artist[j].image[1]["#text"]);
+                //img.addClass("image");
+                var eachBand = container.append(artists);
+                eachBand.addClass("card-image col lg12 col md6 col s3")
+                $("#eachBand").append(eachBand);
+            }
+
+            button =  $("<a>").click(function(){
+                button.attr("href", "buytickets.html");
+            })
+            button.addClass("tickets").text("NEXT EVENT");
+            $("#nextEvent").html(button);
+        })
+    })
+})
+
+
+//Button to follow an artist
+$("#follow").on('click',function(){
+    console.log(typeof(bandsArray));
+    if (typeof bandsArray === "string") {
+        bandsArray = bandsArray.split(",");
+    }       
+        if (bandsArray.includes(bandName)) {            
+            console.log("Band already followed");        
+        } else {
+            bandsArray.push(bandName);
+        }
+   localStorage.setItem("followBand",bandsArray);
+   
+   
+})
+
+
+$("#unfollow").on('click',function(){
+    console.log(typeof(bandsArray));
+    if (typeof bandsArray === "string") {
+        bandsArray = bandsArray.split(",");
+    }       
+    var index = bandsArray.indexOf(bandName);
+    bandsArray.splice(index,1);
+    localStorage.setItem("followBand",bandsArray);
+})
+
+//$('.modalFollow').modal();//
---
+++ follow.js
@@ -0,0 +1,30 @@
+var apiKEY = "105d4ded77051c3ef0322214703f7bb8";
+
+$(document).ready(function(){
+   
+      
+    //FOLLOWING BANDS FUNCTIONS//  
+    var bandName = localStorage.getItem('followBand');
+    bandName = bandName.split(",");
+    for (var i = 0; i < bandName.length; i++) {
+        var queryURL = "https://rest.bandsintown.com/artists/" + bandName[i] + "/?app_id=" + apiKEY;
+        console.log(bandName); 
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function(response){
+            console.log(response);
+            var img = $("<img>");
+            var band = $("<p>");
+            band.addClass("text");
+            var container = $("<div>");
+            container.addClass('cardArtist col s3')
+            var bandFollowed = band.text(response.name);
+            var bandImage = img.attr('src',response.image_url);
+            img.addClass("image");
+            var eachBand = container.append(bandImage,bandFollowed);
+            eachBand.addClass("card-image col s12 m12 l4 ")
+            $("#eachBand").append(eachBand);
+        })
+    }
+})
